17277e2ce84609847e286ce930e927bd7b9e049c
Saima786-Far <saimafarheen786786@gmail.com>
2024-01-06 01:49:52 +0530

    Commit

--- Linear_Search.js
+++ Linear_Search.js
@@ -14,7 +14,15 @@ let result = search(arr, x, n);
 result == -1
 ? console.log("Element are not present in array"):console.log("Element are present in array = " + result);
 
-/*Advantages of Linear Search:
+/*Complexity Analysis of Linear Search:
+Time Complexity:
+
+Best Case: In the best case, the key might be present at the first index. So the best case complexity is O(1)
+Worst Case: In the worst case, the key might be present at the last index i.e., opposite to the end from which the search has started in the list. So the worst-case complexity is O(N) where N is the size of the list.
+Average Case: O(N)
+Auxiliary Space: O(1) as except for the variable to iterate through the list, no other variable is used. 
+
+Advantages of Linear Search:
 Linear search can be used irrespective of whether the array is sorted or not. It can be used on arrays of any data type.
 Does not require any additional memory.
 It is a well-suited algorithm for small datasets.
